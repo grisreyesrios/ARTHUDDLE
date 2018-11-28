aa7bb4a7dcc6b0834f2f65741a79e93e34b957c8
Katharine Doyle <katharinecdoyle@gmail.com>
2018-11-28 18:19:48 +0100

    Set up the dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def dashboard
+    skip_authorization
     @workshops = current_user.workshops
     @bookings = current_user.bookings
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :workshops
+  has_many :bookings
 end
