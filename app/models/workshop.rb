041a8944e51e3da6a7aadeda1aed163b907ac17c
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-28 10:14:50 +0100

    Search bar in container (#15)

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,4 +1,6 @@
 class Workshop < ApplicationRecord
+
+   # include PgSearch
   validates :name, presence: true, uniqueness: true
   validates :area, presence: true
   validates :capacity, numericality: { only_integer: true }
@@ -8,4 +10,8 @@ class Workshop < ApplicationRecord
   # has_many :reviews, through: :bookings, dependent: :destroy
 
   belongs_to :user, optional: true
+
+  # pg_search_scope :search_by_category, against => :category
+
+
 end
