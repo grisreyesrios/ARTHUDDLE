1c09b93b496e6aa1704a2395506a61262b383950
Katharine Doyle <katharinecdoyle@gmail.com>
2018-11-29 18:45:11 +0100

    Finished geocoding map

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,5 +1,6 @@
 class Workshop < ApplicationRecord
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   include PgSearch
   acts_as_votable
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_161109) do
+ActiveRecord::Schema.define(version: 2018_11_29_164201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2018_11_29_161109) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_workshops_on_user_id"
   end
 
