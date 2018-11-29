75fefe30c034384668b4cc8dcf4f9d69d964b5bd
Grisell Reyes Rios <grisell.reyes.rios@gmail.com>
2018-11-29 13:34:11 +0100

    Final search with view (#29)

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,6 +1,5 @@
 class Workshop < ApplicationRecord
 
-  include PgSearch
   validates :name, presence: true, uniqueness: true
   validates :area, presence: true
   validates :capacity, numericality: { only_integer: true }
@@ -10,7 +9,5 @@ class Workshop < ApplicationRecord
   # has_many :reviews, through: :bookings, dependent: :destroy
 
   belongs_to :user, optional: true
-
-  pg_search_scope :search_by_category, :against => :category
 
 end
