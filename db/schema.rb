7ee735828ad1196857ac9d44e90625b38a8b513e
Katharine Doyle <katharinecdoyle@gmail.com>
2018-11-29 19:55:22 +0100

    Changed logo and navbar image

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2018_11_29_164201) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
