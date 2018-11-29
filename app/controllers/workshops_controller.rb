9245d87dcf6b403d028ba85d6e736921d659f0e0
Samuel Beca <samuel.beca@gmail.com>
2018-11-29 18:16:33 +0100

    tested and working

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -4,7 +4,7 @@ class WorkshopsController < ApplicationController
 
   def index
     if params[:query].present?
-      @ẃorkshops = Workshop.where(category: params[:query])
+      @workshops = Workshop.where(category: params[:query])
     else
       @workshops = Workshop.all
     end
