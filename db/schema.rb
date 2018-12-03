b540a2a887e9f03a7f0ac39800a620e7573edb17
Katharine Doyle <katharinecdoyle@gmail.com>
2018-12-03 17:42:34 +0100

    Adjustments to styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_135132) do
+ActiveRecord::Schema.define(version: 2018_12_03_161721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
