ead92306860c3ee6b11d00b8e16ebdc4f1334fd7
Katharine Doyle <katharinecdoyle@gmail.com>
2018-12-06 10:46:56 +0100

    Fixed devise, visitor can now see index and show without signing in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
 
   resources :workshops do
     post :favourited
@@ -8,7 +9,6 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  root to: 'pages#home'
 
   get :dashboard, to: 'users#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
