b55f7ffeecac26aa01c9f7ccbf60c65b22b42bb6
Samuel Beca <samuel.beca@gmail.com>
2018-12-06 13:22:19 +0100

    added fadein test for workshop cards

--- app/assets/javascripts/fadein.js
+++ app/assets/javascripts/fadein.js
@@ -4,5 +4,9 @@ $(document).ready(function() {
     .animate({opacity: 1.0}, 2000);
 });
 
-
+$(document).ready(function() {
+  $('#workshop-card-container')
+    .css('visibility', 'visible')
+    .animate({opacity: 1.0}, 2000);
+});
 
